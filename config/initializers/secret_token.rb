8329af9ac1a1c9562c7cef54861b9f3e4df8b8c1
calico-g <calico@cx.com>
2014-09-03 16:50:53 -0700

    Second commit, gollum rails installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lindyuniverse::Application.config.secret_key_base = 'fd0d852191fd9c513237b59cbc0d2657ce214959fd02eb16be87bd0a6128e79b149598ef6fe201d3ab14837681b9a19386ee496a4cccc8a6162c5c24a773fbf7'
